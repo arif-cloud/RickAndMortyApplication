f1d685cd8238e7a55dd4db7cc1d842a995799092
Arif Demiryas <73065590+arif-cloud@users.noreply.github.com>
2023-09-13 12:58:48 +0300

    Created retrofit interface

---
+++ app/src/main/java/com/example/rickandmortyapplication/data/remote/RickAndMortyApi.kt
@@ -0,0 +1,15 @@
+package com.example.rickandmortyapplication.data.remote
+
+import com.example.rickandmortyapplication.data.remote.dto.CharacterDto
+import com.example.rickandmortyapplication.data.remote.response.CharacterResponse
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+interface RickAndMortyApi {
+    @GET("/character")
+    suspend fun getAllCharacters() : CharacterResponse
+
+    @GET("/character/{character_id}")
+    suspend fun getCharacter(@Path("character_id") characterId : Int) : CharacterDto
+
+}
