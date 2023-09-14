abb31b2bd5d1d33db42399fffdbfc173fcabea97
Arif Demiryas <73065590+arif-cloud@users.noreply.github.com>
2023-09-14 14:43:28 +0300

    Character List State removed

--- app/src/main/java/com/example/rickandmortyapplication/presentation/character_list/CharacterListState.kt
+++
@@ -1,9 +0,0 @@
-package com.example.rickandmortyapplication.presentation.character_list
-
-import com.example.rickandmortyapplication.domain.model.Character
-
-data class CharacterListState(
-    val isLoading : Boolean = false,
-    val data : List<Character>? = null,
-    val error : String = ""
-)
