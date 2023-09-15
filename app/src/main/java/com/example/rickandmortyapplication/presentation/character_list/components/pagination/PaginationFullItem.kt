0fb69ace27b3d8159f78e27522d748cd6ac91e03
Arif Demiryas <73065590+arif-cloud@users.noreply.github.com>
2023-09-15 17:17:11 +0300

    Update paging package name

--- app/src/main/java/com/example/rickandmortyapplication/presentation/character_list/components/pagination/PaginationFullItem.kt
+++ app/src/main/java/com/example/rickandmortyapplication/presentation/character_list/components/pagination/PaginationFullItem.kt
@@ -1,4 +1,4 @@
-package com.example.rickandmortyapplication.presentation.character_list.components.paging
+package com.example.rickandmortyapplication.presentation.character_list.components.pagination
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
